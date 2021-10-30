94954f51a871439c2cac714bc5ddfc4ac68cb020
Niccolò Vianello <vianello.niccolo@gmail.com>
2021-10-30 18:45:44 +0200

    User Location

---
+++ test_ARFoundation/Assets/Scrpts/GPS Location.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GPSLocation : MonoBehaviour
+{
+
+    public float lat;
+    public float lon;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(Input.location.isEnabledByUser) StartCoroutine(GetLocation());
+    }
+
+    private IEnumerator GetLocation()
+    {
+        Input.location.Start();
+        while (Input.location.status == LocationServiceStatus.Initializing) {
+            yield return new WaitForSeconds(0.5f);
+        }
+
+        lat = Input.location.lastData.latitude;
+        lon = Input.location.lastData.longitude;
+
+        yield break;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        lat = Input.location.lastData.latitude;
+        lon = Input.location.lastData.longitude;
+    }
+}
